cb81051e6a3b4042b93b3d712b020aa6258db0b3
goldylucks <adamgoldman7@gmail.com>
2016-07-12 18:15:47 +0200

    bump

--- src/webpackCopyOnDonePlugin.js
+++ src/webpackCopyOnDonePlugin.js
@@ -3,11 +3,11 @@
 const exec = require('child_process').exec;
 
 // toCopy -> array with { src, target }
-const buildToSignerPlugin = function (toCopy) {
+const WebpackCopyOnDonePlugin = function (toCopy) {
   this.toCopy = toCopy
 };
 
-buildToSignerPlugin.prototype.puts = function (err, stdout, stderr) {
+WebpackCopyOnDonePlugin.prototype.puts = function (err, stdout, stderr) {
   console.log('\n\n****************************');
   if (err) {
     console.log('[SIGNER PLUGIN] error ', err);
@@ -20,11 +20,11 @@ buildToSignerPlugin.prototype.puts = function (err, stdout, stderr) {
   console.log('****************************\n');
 };
 
-buildToSignerPlugin.prototype.apply = function (compiler) {
+WebpackCopyOnDonePlugin.prototype.apply = function (compiler) {
   compiler.plugin('done', this.onDone.bind(this));
 };
 
-buildToSignerPlugin.prototype.onDone = function (/* params */) {
+WebpackCopyOnDonePlugin.prototype.onDone = function (/* params */) {
   console.log('\n\n****************************');
   console.log('[SIGNER PLUGIN] WEBPACK DONE');
   console.log('****************************\n');
@@ -34,4 +34,4 @@ buildToSignerPlugin.prototype.onDone = function (/* params */) {
   })
 };
 
-module.exports = buildToSignerPlugin;
+module.exports = WebpackCopyOnDonePlugin;
