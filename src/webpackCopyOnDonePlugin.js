e50357d3520c20a02612c88d32e6c0563e3a35e6
goldylucks <adamgoldman7@gmail.com>
2016-07-12 18:26:14 +0200

    bump

--- src/webpackCopyOnDonePlugin.js
+++ src/webpackCopyOnDonePlugin.js
@@ -7,32 +7,33 @@ const WebpackCopyOnDonePlugin = function (toCopy) {
   this.toCopy = toCopy
 };
 
-WebpackCopyOnDonePlugin.prototype.puts = function (err, stdout, stderr) {
-  console.log('\n\n****************************');
-  if (err) {
-    console.log('[SIGNER PLUGIN] error ', err);
-    return;
-  }
-  console.log(`[SIGNER PLUGIN] copied ${this.src} to ${this.target}`);
-  if (stdout) {
-    console.log('[SIGNER PLUGIN] ', stdout);
-  }
-  console.log('****************************\n');
-};
-
 WebpackCopyOnDonePlugin.prototype.apply = function (compiler) {
   compiler.plugin('done', this.onDone.bind(this));
 };
 
 WebpackCopyOnDonePlugin.prototype.onDone = function (/* params */) {
-  var self = this;
   console.log('\n\n****************************');
   console.log('[SIGNER PLUGIN] WEBPACK DONE');
   console.log('****************************\n');
   this.toCopy.forEach(function(tc) {
     const cpJs = `cp -r ${tc.src} ${tc.target}`;
-    exec(cpJs, self.puts.bind(self));
+    exec(cpJs, this.onCopy(src, target));
   })
 };
+
+WebpackCopyOnDonePlugin.prototype.onCopy = function (src, target) {
+  return function (err, stdout, stderr) {
+    console.log('\n\n****************************');
+    if (err) {
+      console.log('[SIGNER PLUGIN] error ', err);
+      return;
+    }
+    console.log(`[SIGNER PLUGIN] copied ${src} to ${target}`);
+    if (stdout) {
+      console.log('[SIGNER PLUGIN] ', stdout);
+    }
+    console.log('****************************\n');
+  };
+}
 
 module.exports = WebpackCopyOnDonePlugin;
